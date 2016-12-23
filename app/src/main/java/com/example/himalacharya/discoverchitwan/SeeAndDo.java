89bf99d20f0aad0d6c1be9b97afa455dab648fbb
Himal Acharya <Himal Acharya>
2016-12-23 08:41:16 +0545

    After Adding Fragment

--- app/src/main/java/com/example/himalacharya/discoverchitwan/SeeAndDo.java
+++ app/src/main/java/com/example/himalacharya/discoverchitwan/SeeAndDo.java
@@ -2,15 +2,9 @@ package com.example.himalacharya.discoverchitwan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutCompat;
-import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.TextView;
+
 
 import java.util.ArrayList;
-
 public class SeeAndDo extends AppCompatActivity {
 
     @Override
@@ -18,21 +12,10 @@ public class SeeAndDo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_see_and_do);
 
-        //Creating array of activities
-        ArrayList<Location> whatToDo=new ArrayList<>();
-
-        LocationAdapter adapter=new LocationAdapter(this,whatToDo);
-
-        ListView listView= (ListView) findViewById(R.id.seenanddolistView);
-        listView.setAdapter(adapter);
-
-        whatToDo.add(new Location("Elephant Back Safari","Sauraha",R.drawable.family_daughter));
-        whatToDo.add(new Location("Jeep Safari","Kassara",R.drawable.family_mother));
-        whatToDo.add(new Location("Visit Elephant Breeding Centre","Baghmara",R.drawable.family_older_sister));
-        whatToDo.add(new Location("narayani Motor Boat","Jalbire",R.drawable.color_dusty_yellow));
-        whatToDo.add(new Location("Village Walk","Korak",R.drawable.number_ten));
-
-
-
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.activity_see_and_do, new SeeAndDoFragment())
+                .commit();
     }
+
+
 }
---
+++ app/src/main/java/com/example/himalacharya/discoverchitwan/SeeAndDoFragment.java
@@ -0,0 +1,50 @@
+package com.example.himalacharya.discoverchitwan;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SeeAndDoFragment extends Fragment {
+
+
+    public SeeAndDoFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+       View rootView =inflater.inflate(R.layout.activity_see_and_do,container,false);
+
+        //Creating array of activities
+        ArrayList<Location> whatToDo=new ArrayList<>();
+
+        LocationAdapter adapter=new LocationAdapter(getActivity(),whatToDo);
+
+        ListView listView= (ListView) rootView.findViewById(R.id.seenanddolistView);
+        listView.setAdapter(adapter);
+
+        whatToDo.add(new Location("Elephant Back Safari","Sauraha",R.drawable.family_daughter));
+        whatToDo.add(new Location("Jeep Safari","Kassara",R.drawable.family_mother));
+        whatToDo.add(new Location("Visit Elephant Breeding Centre","Baghmara",R.drawable.family_older_sister));
+        whatToDo.add(new Location("narayani Motor Boat","Jalbire",R.drawable.color_dusty_yellow));
+        whatToDo.add(new Location("Village Walk","Korak",R.drawable.number_ten));
+
+        return  rootView;
+
+
+
+    }
+
+}
